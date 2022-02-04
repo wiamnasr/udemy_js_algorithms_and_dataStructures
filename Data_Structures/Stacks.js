4ea60da6e958f99430a933734b221d05fc9a1f8b
Wiam Nasr <wiamnasr@gmail.com>
2022-02-04 04:47:05 +0000

    stacks data structure and its push method implemented

---
+++ Data_Structures/Stacks.js
@@ -0,0 +1,75 @@
+/*
+    Stacks
+
+            >   What is a stack?
+
+            >   What are the  use-cases?
+
+            >   How to implement operations on a stack data structure?
+
+
+            ________________________________________________________________________________________
+
+            ***Stacks and Ques are both data collections, an abstract data structure
+
+            ***Stacks abide by the LIFO principle, Last In First Out
+
+            ***Stacks are used to manage function invocations, Undo/Redo functionality, Routing (the history object e.g. the one used by React), stacks are used as an intermediary in the algorithm
+
+            
+            ___________________________________________________________________________________________
+
+    
+                -----------------------------
+                    PUSHING PSEUDO-CODE
+                -----------------------------
+
+                -   Function should accept value
+
+                -   Create a new node with that value
+
+                -   no nodes in the stack ? set first and last property to newly created node
+
+                -   If there is at least one node, create a var that stores the current first property on the stack
+
+                -   Reset the first property to be the newly created node
+
+                -   Set the next property on the node to be the previously created var
+
+                -   Increment the size of the stack by 1
+
+                -   Return the size
+
+                
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+}
