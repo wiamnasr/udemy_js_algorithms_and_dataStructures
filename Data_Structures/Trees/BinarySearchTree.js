8e626d0b9259d6d1d637103225b124e368d60bd4
Wiam Nasr <wiamnasr@gmail.com>
2022-02-04 07:36:14 +0000

    took notes on trees and binary search trees, implemented BST with insert and find methods with pseudocode and time complexity

---
+++ Data_Structures/Trees/BinarySearchTree.js
@@ -0,0 +1,152 @@
+/*
+    Binary trees & Binary Search Trees (BSTs)
+
+
+    ***Each node can have at most 2 children
+    ***Binary Search Trees are a special case of a binary tree, where each node also can have at most 2 children, on top of that, binary search trees are kept in order (sorted in a certain way)
+    ***In Binary Search Trees, every node to the left of the parent is always less than the parent, and every node to the right of the parent is always greater than the parent
+
+
+    ___________________________________________________________________________________________
+
+    
+                -----------------------------
+                    INSERTING PSEUDO-CODE
+                -----------------------------
+
+                ***Inserting can be done iteratively or recursively
+
+                -   Create a new node
+
+                -   Starting at the root:
+                        >   If there is no root, the root becomes the newNode
+                        >   If there is a root, check if the value of the new node is greater than or less than the value of the root
+                        >   If new node value is greater than root value:
+                                    --> Check to see if there's a node to the right
+                                        ----> If there is, move to that node, and repeat these steps
+                                        ----> If there is no node to the right, add that node as the right property
+                        >   If the new node value is less than the root value:
+                                    --> Check to see if there's a node to the left
+                                        ----> If there is, move to that node and repeat these steps
+                                        ----> If there is not, add that node as the left property
+
+    ___________________________________________________________________________________________
+
+    
+                -----------------------------
+                    FIND PSEUDO-CODE
+                -----------------------------
+                ***Find can be done iteratively or recursively
+
+                -   Starting at the root
+                    > Check if there is a root, if not, done searching
+                    > If there is a root, check if the value of the new node is the value we are searching for, if it is, its found and we're done
+                    > If not, check to see if the value is > || < the value of the root
+                    > If it is greater:
+                        --> Check to see if there is a node to the right
+                            ---->   If there is, move to that node and repeat these steps
+                            ---->   If there isn't we're done searching
+                    > If it is less:
+                        --> Check to see if there is a node to the left
+                            ---->   If there is, move to that node and repeat these steps
+                            ---->   If there isn't we're done searching
+
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+
+      while (true) {
+        //   Accounting for duplicate inserts, not to run into an infinite loop
+        if (value === current.value) return undefined;
+
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+
+    var current = this.root,
+      found = false;
+
+    //   while there is something to loop over and its not found
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    //   returning false if not found
+    if (!found) return undefined;
+    return current;
+  }
+}
+
+// Initializing
+var tree = new BinarySearchTree();
+tree.insert(5);
+tree.insert(3);
+
+tree.insert(500);
+tree.insert(-6);
+tree.insert(30);
+tree.insert(6);
+
+// trying to insert a duplicate => should return undefined
+// console.log(tree.insert(6));
+
+console.log(tree.find(95));
+console.log(tree.find(3));
+console.log(tree.find(30));
+console.log(tree.find(-2));
+
+/*
+          _____________________________________________________________________________________________
+
+          Big O of BST - Best and average case
+
+            - Insertion: O(log n)
+
+            - Searching: O(log n)
+
+            
+*/
---
+++ Data_Structures/Trees/readMe.md
@@ -0,0 +1,121 @@
+<h1>Trees</h1>
+
+<ul>
+    <li>What is a Tree?</li>
+    <li>What's the difference between trees and lists?</li>
+    <li>What are the differences between trees, binary trees and binary search trees?</li>
+    <li>How to implement operations on binary search trees?</li>
+</ul>
+
+<p>Trees are data structures that consists of nodes in a parent/child relationship. We end up with branching structures (tree like) <br></br> Trees, unlike lists are non-linear, many different paths that could be taken, with lists that are linear, there's only one path through</p>
+
+<br></br>
+<br></br>
+
+<h2>There are some rules when talking about trees</h2>
+
+<li>A node can only point to a child (parent/child relationship), we cannot have a child pointing to the parent</li>
+<li>We cannot have a node pointing to a sibling</li>
+<li>Every node is moving "away" from the root node</li>
+<li>The root is the top node of the tree</li>
+<li>child node is a node that s directly connected to another node when moving away from the root</li>
+<li>a parent node has the converse notion of a child node</li>
+<li>Siblings are a group of nodes from the same parent</li>
+<li>A leaf, is a node with no children</li>
+<li>Edge is the actual arrow (the connection) between one node and another</li>
+
+<br></br>
+<br></br>
+
+<h2>Real world use-cases of trees</h2>
+
+> ### HTML/DOM
+
+> ### Network Routing --> Finding shortest path
+
+> ### Abstract Syntax Trees --> A way of describing the syntax of a programming language using a tree structure
+
+> ### Artificial Intelligence --> ML for example a mini max tree (building an ai tool for a tic-tac toe game that anticipates possible moves and what options could happen as a best possible next move ----> decision tree)
+
+> ### Computer Folders modeling in an OS
+
+> ### JSON --> parsed responses from api data for instance, there is a code that is traversing and creating a tree like structure with parent and child nodes, but only a single root
+
+<br></br>
+
+<h2>Binary trees & Binary Search Trees (BSTs)</h2>
+
+<ul>
+    <li>Each node can have at most 2 children</li>
+    <li>Binary Search Trees are a special case of a binary tree, where each node also can have at most 2 children, on top of that, binary search trees are kept in order (sorted in a certain way)</li>
+    <li>In Binary Search Trees, every node to the left of the parent is always less than the parent, and every node to the right of the parent is always greater than the parent</li>
+    
+</ul>
+
+<br></br>
+<br></br>
+
+> ## Binary Search Tree Class and Node Class
+
+### Syntax:
+
+```JavaScript
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
+
+
+// Initializing
+var tree = new BinarySearchTree();
+tree.root = new Node(20);
+
+```
+
+<p></p>
+
+> ###
+
+```JavaScript
+
+
+
+```
+
+<br></br>
+
+###
+
+<br></br>
+
+> ###
+
+```JavaScript
+
+
+
+```
+
+<br></br>
+
+> ###
+
+<br></br>
+
+```JavaScript
+
+
+
+```
